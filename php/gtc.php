a5b5509e07af9b9bcd7586cc8dbd6eda92e75cc0
Erick <esericksanc@gmail.com>
2017-01-07 12:10:57 -0800

    format csv into php

--- php/gtc.php
+++ php/gtc.php
@@ -5,8 +5,18 @@ if(!ini_set('default_socket_timeout', 15)) echo "<!-- unable to change socket ti
 
 if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
 	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-		$spreadsheet_data[] = $data;
-		print_r($data);
+
+		$t = "\t";
+		//TODO parse into correct format >>section, days, time, instructor, campus, room, units, status, total seats, used seats, remaining seats, date begin ends, date final
+
+		//TODO DONE remove any slashes (want MWTTh)
+		$days = preg_replace("/\//", "", $data[2]);
+
+		$course = $t . $data[0] . $t . $data[1] . $t . $days . $t . $data[3] . $t . $data[4] . $t . $data[5] . $t . $data[6] . $t . $data[7] . $t . $t . $t . $t . $t . $data[8] . $t . $data[9] . "\t\n\t \t" . $data[10];
+		echo $course;
+		//save into an array of gtc courses
+		$spreadsheet_data[] = $course;
+
 		echo "<br><br>";
 	}
 	echo "<br><br>";
