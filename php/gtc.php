3e3d575f781c86c1d1efe9a90c035f69e9d84ca7
Erick <esericksanc@gmail.com>
2017-01-09 21:57:57 -0800

    search and echo the formatted course

--- php/gtc.php
+++ php/gtc.php
@@ -3,49 +3,58 @@ $spreadsheet_url="https://docs.google.com/spreadsheets/d/1bI3gCleBzYX7Euz7Wvu5nJ
 
 if(!ini_set('default_socket_timeout', 15)) echo "<!-- unable to change socket timeout -->";
 
-$selected_course = "GtC Government";
+$selected_course = "GtC Study Lab";
 
 if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
-	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-
-		$t = "\t";
-		$b = " "; //blank information for missing attributes from the spreadsheet_url
-		//TODO parse into correct format >>section, days, time, instructor, campus, room, units, status, total seats, used seats, remaining seats, date begin ends, date final
-
-		//TODO DONE remove any slashes (want MWTTh)
-		$days = preg_replace("/\//", "", $data[2]);
-
-		$course_title = "c";
-		if ($data[0] == "")
-			$course_title = "";
-		$course = $t . $course_title . $t . $data[1] . $t . $days . $t . $data[3] . $t . $data[4] . $t . $data[5] . $t . $data[6] . $t . $data[7] . $t . $t . $t . $t . $t . $data[8] . $t . $data[9] . "\t\n\t \t" . $data[10];
-		// echo $course;
-		//save into an array of gtc courses_temp
-		$courses_temp[] = $course;
-
-		// echo "<br><br>";
-	}
-
-	// echo "<hr>";
+  $skip = true;
 
 	$courses_temp;
 	$courses;
 	$index = 0;
 
+	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+		if ($skip)
+		{ //Skip header line
+		    $skip = false;
+		    continue;
+		}
+
+		$t = "\t";
+		$b = " "; //blank information for missing attributes from the spreadsheet_url
+
+        //format >>section, days, time, instructor, campus, room, units, status, total seats, used seats, remaining seats, date begin ends, date final
+
+		$days = preg_replace("/\//", "", $data[2]);
+
+		$course = array( $data[0] , $t . $data[1] . $t . $days . $t . $data[3] . $t . $data[4] . $t . $data[5] . $t . $data[6] . $t . $data[7] . $t . $t . $t . $t . $t . $data[8] . $t . $data[9] . "\t\n\t \t" . $data[10] );
+
+		//save into an array of gtc courses_temp
+		$courses_temp[] = $course;
+
+	}
+
 	foreach ($courses_temp as $course) {
-		if( $course[1] /*course title*/ == "c")
+		if( $course[0] /*course title*/ != "")
 		{ // the course is fine as is
-			$courses[$index] = substr( $course, 2);
+			$courses[$index] = $course;
 			$index += 1;
 		}
 		else
 		{ // it is part of a larger course and needs to be concatenated with the previous course
-			$courses[$index -1] .= "\n\t" . $course;
+			$courses[$index -1][1] .= "$^$" . $course[1];
 		}
 	}
 
-	echo $courses[4];
-	// echo $course . "<br><br>";
+	$return;
+
+	foreach ($courses as $course) {
+		if ($selected_course == $course[0])
+			$return = $course[1];
+
+	}
+
+	echo $return;
+
 	fclose($handle);
 }
 else
